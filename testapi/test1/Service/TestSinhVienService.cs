804552dc9e6606180cc7e83a960ff00a4a181cdc
phuc14022001 <106235647+phuc14022001@users.noreply.github.com>
2023-02-28 17:25:15 +0700

    phuc

---
+++ testapi/test1/Service/TestSinhVienService.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using test1.Data;
+using testapi.data;
+using testapi.servces;
+
+namespace test1.Service
+{
+    public class TestSinhVienService:IDisposable
+    {
+        private readonly MyDbContext _myDbContext;
+        public TestSinhVienService() 
+        {
+            var options = new DbContextOptionsBuilder<MyDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            _myDbContext = new MyDbContext(options);
+
+            _myDbContext.Database.EnsureCreated();
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnSinhienController()
+        {
+            _myDbContext.sinhViens.AddRange(SinhViendata.GetSinhViens());
+            _myDbContext.SaveChangesAsync();
+            var sut = new SingVienReposetory(_myDbContext);
+
+            var result =sut.GetAll();
+
+            result.Should().HaveCount(SinhViendata.GetSinhViens().Count);
+
+        }
+
+        public void Dispose()
+        {
+            _myDbContext.Database.EnsureCreated();
+
+            _myDbContext.Dispose();
+        }
+    }
+}
---
+++ testapi/testapi/Controllers/SinhVienController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testapi.data;
+using testapi.servces;
+
+namespace testapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SinhVienController : ControllerBase
+    {
+        private readonly ISinhVien _iSinhVien;
+
+        public SinhVienController(ISinhVien sinhVien)
+        {
+            _iSinhVien=sinhVien;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var a= _iSinhVien.GetAll();
+                if (a.Count==0)
+                {
+                    return NoContent();
+                }
+                return Ok(a);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                var data = _iSinhVien.SinhVienByGet(id);
+                if (data == null) 
+                    {
+                        return NotFound();
+                    }
+                return Ok(_iSinhVien.SinhVienByGet(id));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, SinhVien s)
+        {
+            if (id != s.Id) return BadRequest();
+            try
+            {
+                _iSinhVien.update(s);
+                return NoContent(); // thông báo thành công và trả về kết quả ( 204 )
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult delete(int id)
+        {
+            try
+            {
+                _iSinhVien.delete(id);
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+        [HttpPost]
+        public IActionResult Add(SinhVien s)
+        {
+            try
+            {
+
+                return Ok(_iSinhVien.Add(s));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
